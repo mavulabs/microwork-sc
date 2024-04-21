27690571997747948eefdb751a3a9c5d60ba5656
Suvadra-Barua <suvadra@universalmachine.io>
2024-04-21 18:38:54 +0600

    Add JobInfo contract

---
+++ contracts/JobInfo.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.0;
+
+contract JobContract {
+    bytes public jobDescription;
+    bytes32 public typeOfJob;
+    uint256 public deadline;
+    // address[] public assignedTo;
+    uint256[] public tasks;
+    bool public jobStatus; // InProgress: true, Done: false
+    mapping(uint256 => address) taskIdToAssignee;
+
+    event AssigneeChanged(address newAssignedTo);
+    event DeadlineUpdated(uint256 newDeadline);
+    event TaskCreated(uint256 indexed taskId, address indexed assignee);
+    event TaskAssigned(uint256 indexed taskId, address indexed assignee);
+
+    // modifier onlyAssigned() {
+    //     bool found = false;
+    //     for (uint256 i = 0; i < assignedTo.length; i++) {
+    //         if (assignedTo[i] == msg.sender) {
+    //             found = true;
+    //             break;
+    //         }
+    //     }
+    //     require(found, "You are not assigned to this job");
+    //     _;
+    // }
+
+    constructor(
+        bytes memory _jobDescription,
+        bytes32 _typeOfJob,
+        uint256 _deadline
+    ) {
+        jobDescription = _jobDescription;
+        typeOfJob = _typeOfJob;
+        deadline = _deadline;
+        jobStatus = true; // By default, job is set to InProgress
+    }
+
+    function getJobInfo()
+        public
+        view
+        returns (bytes memory, bool, bytes32, uint256)
+    {
+        return (jobDescription, jobStatus, typeOfJob, deadline);
+    }
+
+    function createTask(address _assignedTo) public {
+        uint256 _taskId = tasks.length;
+        tasks.push(_taskId);
+        taskIdToAssignee[_taskId] = _assignedTo;
+        emit TaskCreated(_taskId, _assignedTo);
+    }
+}
