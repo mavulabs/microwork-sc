b66f930109f795727b98a93918d9edaec4559829
Suvadra-Barua <suvadra@universalmachine.io>
2024-08-15 19:57:15 +0600

    Add JobDetails struct and batchRegisterJob

--- contracts/JobFactory.sol
+++ contracts/JobFactory.sol
@@ -4,33 +4,40 @@ import "./JobInfo.sol";
 
 contract JobFactory {
     address[] public jobContracts;
+    struct JobDetails {
+        bytes jobDescription;
+        bytes32 typeOfJob;
+        uint256 deadline;
+        address _cusdAddress;
+        address _mavuCoinAddress;
+        address _scoreAddress;
+        uint256 _cusdRewardAmount;
+        uint256 _mavuRewardAmount;
+        uint256 _scoreRewardAmount;
+    }
 
     event JobRegistered(address indexed jobContract);
 
-    function registerJob(
-        bytes memory jobDescription,
-        bytes32 typeOfJob,
-        uint256 deadline,
-        address _cusdAddress,
-        address _mavuCoinAddress,
-        address _scoreAddress,
-        uint256 _cusdRewardAmount,
-        uint256 _mavuRewardAmount,
-        uint256 _scoreRewardAmount
-    ) public {
+    function registerJob(JobDetails calldata _jobInfo) public {
         JobInfo newJob = new JobInfo(
-            jobDescription,
-            typeOfJob,
-            deadline,
-            _cusdAddress,
-            _mavuCoinAddress,
-            _scoreAddress,
-            _cusdRewardAmount,
-            _mavuRewardAmount,
-            _scoreRewardAmount
+            _jobInfo.jobDescription,
+            _jobInfo.typeOfJob,
+            _jobInfo.deadline,
+            _jobInfo._cusdAddress,
+            _jobInfo._mavuCoinAddress,
+            _jobInfo._scoreAddress,
+            _jobInfo._cusdRewardAmount,
+            _jobInfo._mavuRewardAmount,
+            _jobInfo._scoreRewardAmount
         );
         jobContracts.push(address(newJob));
         emit JobRegistered(address(newJob));
+    }
+
+    function batchRegisterJob(JobDetails[] calldata _jobInfos) public {
+        for (uint256 i = 0; i < _jobInfos.length; i++) {
+            registerJob(_jobInfos[i]);
+        }
     }
 
     function getCompletedJobs() public view returns (address[] memory) {
