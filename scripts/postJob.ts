9bc3504327f462ac9bc78a0f1e16239b54260151
Suvadra-Barua <suvadra@universalmachine.io>
2024-08-22 21:34:17 +0600

    Read event logs

--- scripts/postJob.ts
+++ scripts/postJob.ts
@@ -24,6 +24,27 @@ async function main() {
     const tx = await JobFactoryContract.registerJob(jobDetails);
     await tx.wait();
     console.log(`Tx Hash: ${tx.hash}`);
+    // const txHash =
+    //     "0xfdad4b68d8aeb5199cfa7792dae8ba9f20c40e0249b0fbb6b75039f5efa898a0";
+    const txHash = tx.hash;
+
+    const provider = await hre.ethers.provider;
+    const txReceipt = await provider.getTransactionReceipt(txHash);
+    // const contractInterface = new hre.ethers.Interface(jobFactoryAbi);
+    console.log({ txReceipt });
+    console.log(txReceipt.logs);
+    const contractInterface = JobFactory.interface;
+    for (const log of txReceipt.logs) {
+        try {
+            const event = contractInterface.parseLog(log);
+            console.log("Event:", event);
+            const JobInfoAddress = event.args[0];
+            console.log({ JobInfoAddress });
+        } catch (error) {
+            // Ignore events that don't match the expected signature
+            console.log({ error });
+        }
+    }
 }
 
 main()
