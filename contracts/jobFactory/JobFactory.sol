76ffaa47352a02ac5be343444235d858fe905743
Suvadra-Barua <suvadra@universalmachine.io>
2024-12-02 21:11:14 +0600

    Add JobFactory contract

---
+++ contracts/jobFactory/JobFactory.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./UpgradeableJobFactory.sol";
+import "../jobInfo/JobInfo.sol";
+
+contract JobFactory is UpgradeableJobFactory {
+    uint256 public constant MAX_BATCH_SIZE = 50;
+
+    mapping(address => address[]) public creatorToJobs;
+    uint256 public totalJobs;
+
+    function initialize() public initializer {
+        __UpgradeableJobFactory_init();
+        totalJobs = 0;
+    }
+
+    function registerJob(
+        JobDetails calldata _jobInfo
+    ) public nonReentrant returns (address) {
+        _validateJobDetails(_jobInfo);
+
+        JobInfo newJob = new JobInfo();
+        newJob.initialize(
+            _jobInfo.jobCreator,
+            _jobInfo.typeOfJob,
+            _jobInfo.rewardTokens,
+            _jobInfo.rewardAmount
+        );
+
+        address jobAddress = address(newJob);
+        _addJobContract(jobAddress, _jobInfo.jobCreator);
+        creatorToJobs[_jobInfo.jobCreator].push(jobAddress);
+        totalJobs++;
+
+        return jobAddress;
+    }
+
+    function getCompletedJobs() public view returns (address[] memory) {
+        return _filterJobs(false);
+    }
+
+    function getInProgressJobs() public view returns (address[] memory) {
+        return _filterJobs(true);
+    }
+
+    function getJobsByCreator(
+        address creator
+    ) public view returns (address[] memory) {
+        return creatorToJobs[creator];
+    }
+
+    function _filterJobs(
+        bool inProgress
+    ) internal view returns (address[] memory) {
+        uint256 count = 0;
+        for (uint256 i = 0; i < jobContracts.length; i++) {
+            if (JobInfo(jobContracts[i]).jobStatus() == inProgress) {
+                count++;
+            }
+        }
+
+        address[] memory filteredJobs = new address[](count);
+        uint256 index = 0;
+        for (uint256 i = 0; i < jobContracts.length; i++) {
+            if (JobInfo(jobContracts[i]).jobStatus() == inProgress) {
+                filteredJobs[index] = jobContracts[i];
+                index++;
+            }
+        }
+
+        return filteredJobs;
+    }
+}
