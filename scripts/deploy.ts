dbfc99b0faa9a077b92c3cc700b321f614496c4d
Suvadra-Barua <suvadra@universalmachine.io>
2025-02-13 19:30:16 +0600

    Add contract verification

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,31 +6,29 @@ import { ethers, upgrades } from "hardhat";
 const deployJobFactory = async function () {
     console.log("Deploying JobFactory contract...");
     const JobFactory = await ethers.getContractFactory("JobFactory");
-    console.log("hello");
 
     const jobFactoryProxy = await upgrades.deployProxy(JobFactory, [], {
         initializer: "initialize",
         kind: "uups",
     });
-    console.log("hello2");
     await jobFactoryProxy.waitForDeployment();
     const jobFactoryAddress = await jobFactoryProxy.getAddress();
 
     console.log("JobFactory deployed to:", jobFactoryAddress);
 
     // Verify contract on Etherscan if not on localhost
-    // if (hre.network.name !== "hardhat" && hre.network.name !== "localhost") {
-    //   console.log("Verifying contract on Etherscan...");
-    //   try {
-    //     await hre.run("verify:verify", {
-    //       address: jobFactoryAddress,
-    //       constructorArguments: [],
-    //     });
-    //     console.log("Contract verified successfully");
-    //   } catch (error) {
-    //     console.log("Error verifying contract:", error);
-    //   }
-    // }
+    if (hre.network.name !== "hardhat" && hre.network.name !== "localhost") {
+        console.log("Verifying contract on Etherscan...");
+        try {
+            await hre.run("verify:verify", {
+                address: jobFactoryAddress,
+                constructorArguments: [],
+            });
+            console.log("Contract verified successfully");
+        } catch (error) {
+            console.log("Error verifying contract:", error);
+        }
+    }
 
     return true;
 };
